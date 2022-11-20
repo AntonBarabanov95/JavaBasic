9dce840be046d75eb73460f943879f07411a2b1b
Anton Barabanov <anton.barabanov95@gmail.com>
2022-11-20 20:59:57 +0300

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Целый числа
+        byte varByte = 1; //-128 .. 127
+        short varShort = 1; //-32768 .. 32767
+        int varInt = 1;
+        long varLong = 1L;
+
+        //Числа с плавающей точкой
+        float varFloat = 0.0f;
+        double varDouble = 0.0d;
+
+        //Логический
+        boolean varBoolean = true;
+
+        //Символьный
+        char varChar = 'c';
+
+        String varString = "Hello world!";
+
+    }
+}
